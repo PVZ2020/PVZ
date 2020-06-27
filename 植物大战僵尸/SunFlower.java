6fd61b6f3be490b1995b2594f0d138ff2167c04d
Scarels <3527017847@qq.com>
2020-06-27 09:46:46 +0800

    首次提交

---
+++ 植物大战僵尸/SunFlower.java
@@ -0,0 +1,14 @@
+package ÷≤ŒÔ¥Û’ΩΩ© ¨;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class SunFlower extends Plants{
+
+	SunFlower(int m,int n,Graphics g,Image img){
+		super(m,n,g,img);
+	}
+	public void draw(Graphics g){
+		g.drawImage(img, 350+n*115, 150+m*175, 130, 175, null);
+	}
+}
